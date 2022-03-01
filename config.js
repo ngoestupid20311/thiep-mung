a3f89c2b7b97428a6b7bcb63347b609b8661d4d8
ngoestupid20311 <100668792+ngoestupid20311@users.noreply.github.com>
2022-03-01 13:42:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ const contentCard = 'và đây là Hồng Bích người con gái hiền lành';
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png.JPG';
+const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
